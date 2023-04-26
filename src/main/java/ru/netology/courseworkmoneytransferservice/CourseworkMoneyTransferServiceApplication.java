7059e623f1272411bbbac20186697ba1606ed206
Daria Suntcova <batareyka23@gmail.com>
2023-04-26 13:40:15 +0300

    init commit

---
+++ src/main/java/ru/netology/courseworkmoneytransferservice/CourseworkMoneyTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.courseworkmoneytransferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkMoneyTransferServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseworkMoneyTransferServiceApplication.class, args);
+    }
+
+}
